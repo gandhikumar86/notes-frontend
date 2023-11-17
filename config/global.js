7206c2e3e70a7e6fb62b0b70130467c4dad36313
Gandhi Kumar <gandhikumar86@gmail.com>
2023-11-17 23:03:29 +0530

    API live url added

--- config/global.js
+++ config/global.js
@@ -1,4 +1,4 @@
-//const API_URL = "https://auth-backend-6p89.onrender.com";
-const API_URL = "http://localhost:4000";
+//const API_URL = "http://localhost:4000";
+const API_URL = "https://notes-backend-nc04.onrender.com";
 
 export default API_URL;
